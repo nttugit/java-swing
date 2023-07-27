19e8599bd2cfde79bae7822e752ff87c6a190c96
nttu <nttugpt@gmail.com>
2023-07-27 23:50:08 +0700

    Lesson 97: Notify with JOptionPane

--- src/lesson_95_radio_and_checkbox/MenuController.java
+++ src/lesson_95_radio_and_checkbox/MenuController.java
@@ -6,6 +6,7 @@ import java.util.Enumeration;
 
 import javax.swing.AbstractButton;
 import javax.swing.JCheckBox;
+import javax.swing.JOptionPane;
 
 public class MenuController implements ActionListener {
 	private MenuView menuView;
@@ -30,5 +31,25 @@ public class MenuController implements ActionListener {
 		this.menuView.menuModel.setOption(mainToppings +", " + subToppings);
 		this.menuView.menuModel.calculateTotalPrice();
 		this.menuView.showTotalPrice();
+		
+		// Show notification
+		String userCashString = JOptionPane.showInputDialog(
+				this.menuView,
+				this.menuView.userChoiceLabel.getText() + ". Tiền khách đưa: ", 
+				"Thông báo", JOptionPane.PLAIN_MESSAGE);
+		
+		try {
+			double userCash = Double.parseDouble(userCashString);
+			double totalPrice = this.menuView.menuModel.getTotal();
+			if(userCash < totalPrice) {
+				JOptionPane.showMessageDialog(this.menuView, "Tiền khách đưa không đủ", "Lỗi", JOptionPane.ERROR_MESSAGE);
+				return;
+			}
+			JOptionPane.showMessageDialog(this.menuView, "Tiền thừa: " + (userCash - totalPrice), "Thông báo", JOptionPane.INFORMATION_MESSAGE);
+
+		} catch (Exception e2) {
+			JOptionPane.showMessageDialog(this.menuView, "Nhập dữ liệu sai!", "Lỗi", JOptionPane.ERROR_MESSAGE);
+		}
+
 	}
 }
